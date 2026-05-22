1f745cb9078e290d6e7c0b2eb9046ce85459f25e
aarush <globalaarush04@gmail.com>
2026-05-22 19:59:15 +0530

    port update

--- app.py
+++ app.py
@@ -26,7 +26,9 @@ def get_db_connection():
         host=os.getenv("MYSQLHOST"),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE")
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT")),
+        connection_timeout=10
     )
 
 
